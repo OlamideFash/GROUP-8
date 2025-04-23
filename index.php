99520ef29ad615a0e3b72c141869ba93a70de127
OlamideFash <fashoranti003@gmail.com>
2025-04-23 12:32:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Group 8 PHP</title>
+</head>
+<body>
+    <h1>Group 8 PHP</h1>
+    <pre>
+       88888
+      8     8
+      8     8
+       88888
+      8     8
+      8     8
+       88888
+    </pre>
+    <p>SHA256 hash of "Group 8":</p>
+    <p>
+        <?php
+            print hash('sha256', 'Group 8');
+        ?>
+    </p>
+    <p><a href="fail.php">Click here to run fail</a></p>
+    <p><a href="check.php">Click here to run check</a></p>
+</body>
+</html>
